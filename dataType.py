7cfaa722bccd3bd1a3e63ecfb19e508bab4e3cd9
Robin1232123 <53208119+Robin1232123@users.noreply.github.com>
2019-07-23 15:26:38 +0900

    Create dataType.py

---
+++ dataType.py
@@ -0,0 +1,19 @@
+# print(1)
+# print(1.321)
+# print("Nobin")
+# print('Ryan')
+# print("Hi my name is Ryan !!")
+# print(True)
+# print(False)
+# print (6> 10)
+#
+# print( 3 * 15 / 9)
+# print ( 3 == 3 )
+# print ( 4 != 5)
+# print (3 > 4 and 6 < 10)
+# print (not (3 > 4) and 6 < 10)
+
+print (2 + 2 * 2)
+print (   (2 + 2 )  * 2 )
+
+# This is basically end of today's python lesson
